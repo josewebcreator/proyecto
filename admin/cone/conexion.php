472a41b98342ecc418554290ae23132295364ad9
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-06-23 18:00:42 -0400

    Update conexion.php

--- admin/cone/conexion.php
+++ admin/cone/conexion.php
@@ -7,11 +7,12 @@ $db = "proyecto";
 
 $mysqli = new mysqli($servidor, $usuario, $clave, $db);
 
+/*
 $query = $mysqli->query("SELECT * from blog");
 
 $res= $query->fetch_object();
 
 echo $res->img;
-
+*/
 
 ?>
