d867201b9e11b42bebee00ca7f0f30494022ff97
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-06-23 11:29:26 -0400

    Update funciones.js

--- admin/publicar/js/funciones.js
+++ admin/publicar/js/funciones.js
@@ -2,13 +2,22 @@ $(document).ready(
     $("#formulario").submit(function (e) { 
         e.preventDefault() 
         $.ajax({
-            type: "POST",
-            url: "../publicar.php",
-            data: FormData,
-            dataType: "dataType",
-            success: function (response) {
-                
+            type: 'POST',
+            url: 'submit.php',
+            data: new FormData(this),
+            contentType: false,
+            cache: false,
+            processData:false,
+            success: function(msg){
+                $('.statusMsg').html('');
+                if(msg == 'ok'){
+                    $('#formulario')[0].reset();
+                    $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Form data submitted successfully.</span>');
+                }else{
+                    $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Some problem occurred, please try again.</span>');
+                }
             }
+
         });
     })
 )
