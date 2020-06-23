f440f8feeb9c6d22018ac606c4b728a6370b3964
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-06-23 17:41:10 -0400

    Update publicar.php

--- admin/publicar/publicar.php
+++ admin/publicar/publicar.php
@@ -2,6 +2,32 @@
 
 require("../cone/conexion.php");
 
-
+if(!empty($_POST['escrito']) || !empty($_POST['titulo']) || !empty($_FILES['imagen']['name'])){
+    $uploadedFile = '';
+    if(!empty($_FILES["file"]["type"])){
+        $fileName = time().'_'.$_FILES['imagen']['name'];
+        $valid_extensions = array("jpeg", "jpg", "png");
+        $temporary = explode(".", $_FILES["imagen"]["name"]);
+        $file_extension = end($temporary);
+        if((($_FILES["hard_file"]["type"] == "image/png") || ($_FILES["imagen"]["type"] == "image/jpg") || ($_FILES["imagen"]["type"] == "image/jpeg")) && in_array($file_extension, $valid_extensions)){
+            $sourcePath = $_FILES['imagen']['tmp_name'];
+            $targetPath = "uploads/".$fileName;
+            if(move_uploaded_file($sourcePath,$targetPath)){
+                $uploadedFile = $fileName;
+            }
+        }
+    }
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    
+    //include database configuration file
+    include_once 'dbConfig.php';
+    
+    //insert form data in the database
+    $insert = $db->query("INSERT form_data (name,email,file_name) VALUES ('".$name."','".$email."','".$uploadedFile."')");
+    
+    echo $insert?'ok':'err';
+}
 
 ?>
