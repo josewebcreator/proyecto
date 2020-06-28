0d1ffc96375982bdb97eaf7c63b1548a94879160
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-06-28 13:53:47 -0400

    Update funciones.js

--- admin/publicar/js/funciones.js
+++ admin/publicar/js/funciones.js
@@ -36,9 +36,13 @@ $(document).ready(function(){
 
 
     //incrustacion de parrado en el formulario
-
+    let cuenta = 0;
+    
     $("#btn-incrustar").click(function (e) {
         e.preventDefault();
-        $("#p_secundarios").append("<p>hola</p>");
+        cuenta += 1;
+        $("#f_blog ul").append("<li>Titulo parrafo " + cuenta + "<br><input type=\"text\" name=\"t_parrafo" + cuenta+"\" id=\"titulo_parrafo\"><br></li><li>parrafo " + cuenta + "<br><textarea name=\"parrafo"+ cuenta +"\" id=\"texto_parrafo\" cols=\"30\" rows=\"10\"></textarea></li><li>imagen "+ cuenta +" <br><input type=\"file\" name=\"imagen"+ cuenta +"\"></li>");
+
+        
     })
 })
