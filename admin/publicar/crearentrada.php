894d2d28183ee213cd761bc7b81c24e8f04c3556
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-06-28 21:50:14 -0400

    Update crearentrada.php

--- admin/publicar/crearentrada.php
+++ admin/publicar/crearentrada.php
@@ -13,21 +13,19 @@
         <ul>
 
             <li>
-                Titulo <br><input type="text" name="titulo_entrada" id="titulo_entrada"><br>
-            </li>
-            <li>
+                Titulo <br><input type="text" name="titulo_entrada" id="titulo_entrada" value="nulo"><br>
+            
                 parrafo Principal <br>
                 <textarea name="parrafo" id="texto_parrafo" cols="30" rows="10"></textarea>
-            </li>
-            <li>
+            
                 imagen de cabecera <br>
-                <input type="file" name="imagen_cabecera">
+                <input type="file" name="imagen_cabecera" id="imagen_cabecera">
             </li>
         </ul>
         <div id="p_sedundarios"></div><br>
         haga clic para incrustar otro parrafo
         <input type="button" value="" name="incrustar" id="btn-incrustar">
-
+        <input type="submit" value="enviar" name="enviar">
     </form>
     <script src="js\jquery.js"></script>
     <script src="js\funciones.js"></script>
