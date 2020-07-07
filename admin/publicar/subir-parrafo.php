a1b62a42ccaa3255dc160ffcba6f8cb2bc25fc9a
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-07 18:26:33 -0400

    Update subir-parrafo.php

---
+++ admin/publicar/subir-parrafo.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+    require("../cone/conexion.php");
+
+
+
+    if(!empty($_POST['titulo_entrada']) || !empty($_POST['parrafo']) || !empty($_FILES['imagen_cabecera']['name'])){
+    $uploadedFile = '';
+    if(!empty($_FILES["imagen_cabecera"]["type"])){
+        $fileName = time().'_'.$_FILES['imagen_cabecera']['name'];
+        $valid_extensions = array("jpeg", "jpg", "png");
+        $temporary = explode(".", $_FILES["imagen_cabecera"]["name"]);
+        $file_extension = end($temporary);
+        if((($_FILES["imagen_cabecera"]["type"] == "image/png") || ($_FILES["imagen_cabecera"]["type"] == "image/jpg") || ($_FILES["imagen_cabecera"]["type"] == "image/jpeg")) && in_array($file_extension, $valid_extensions)){
+            $sourcePath = $_FILES['imagen_cabecera']['tmp_name'];
+            $targetPath = "uploads/".$fileName;
+            if(move_uploaded_file($sourcePath,$targetPath)){
+                $uploadedFile = $fileName;
+            }
+        }
+    }
+
+
+    $ent_titulo=$_POST['t_entrada'];
+    $consulta= $mysqli->query("SELECT `id` FROM `entrada_blog` WHERE `titulo` = '$ent_titulo'");
+    $res = $consulta->fetch_object();
+    $id_titulo= $res->id; 
+    $escrito = $_POST['parrafo'];
+    $titulo = $_POST['titulo_entrada'];
+    $footer = $_POST['foto-footer'];
+    $len = "es";
+    
+    //insert form data in the database
+    $insert = $mysqli->query("INSERT into `entrada_blog` (`lenguaje`, `titulo`, `imagen_central`, `foto_footer`, `texto`) VALUES ( '".$len."','".$titulo."','".$uploadedFile."','".$footer."','".$escrito."')");
+    
+
+    }
+
+?>
