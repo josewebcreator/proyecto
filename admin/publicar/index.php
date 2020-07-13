7779753a29a39331fe7c17f0ab01a4ccb9a6a836
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-13 17:14:10 -0400

    Update index.php

--- admin/publicar/index.php
+++ admin/publicar/index.php
@@ -1,19 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Crear entrada</title>
 </head>
+
 <body>
-    <form action="" method="post" id="formulario">
-        titulo<input type="text" name="titulo"><br>
-        <textarea name="escrito" id="" name="escrito" cols="30" rows="10"></textarea><br>
-        seleccione una imagen <br>
-        <input type="file" name="imagen" id="imagen"><br>
-        <input type="submit" value="enviar">
-    </form>
-<script src="js/jquery.js"></script>
-<script src="js/funciones.js"></script>
+    <ul id="crea-blog">
+        <li>
+            <form action="" class="p-principal">
+                Titulo <br><input type="text" name="titulo_entrada" id="titulo_entrada" value="nulo"><br>
+
+                parrafo Principal <br>
+                <textarea name="parrafo" id="texto_parrafo" cols="30" rows="10"></textarea>
+
+                imagen de cabecera <br>
+                <input type="file" name="imagen_cabecera" id="imagen_cabecera">
+
+                footer foto <br>
+                <textarea name="foto-footer" id="foto-footer" cols="30" rows="10"></textarea>
+            </form>
+        </li>
+    </ul>
+    <br>
+    haga clic para incrustar otro parrafo
+    <input type="button" value="incrustar" name="incrustar" id="btn-incrustar">
+    <input type="button" value="enviar" name="enviar" id="btn-enviar">
+
+    <script src="js\jquery.js"></script>
+    <script src="js\funciones.js"></script>
 </body>
+
 </html>
