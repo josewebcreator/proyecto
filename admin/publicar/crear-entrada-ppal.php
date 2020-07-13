dc98ca5c235f7d37da36e0e608516bd6f8a1fdcf
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-13 16:48:34 -0400

    Update crear-entrada-ppal.php

--- admin/publicar/crear-entrada-ppal.php
+++ admin/publicar/crear-entrada-ppal.php
@@ -2,30 +2,34 @@
 
     require("../cone/conexion.php");
     if(!empty($_POST['titulo_entrada']) || !empty($_POST['parrafo']) || !empty($_FILES['imagen_cabecera']['name'])){
-    $uploadedFile = '';
-    if(!empty($_FILES["imagen_cabecera"]["type"])){
-        $fileName = time().'_'.$_FILES['imagen_cabecera']['name'];
-        $valid_extensions = array("jpeg", "jpg", "png");
-        $temporary = explode(".", $_FILES["imagen_cabecera"]["name"]);
-        $file_extension = end($temporary);
-        if((($_FILES["imagen_cabecera"]["type"] == "image/png") || ($_FILES["imagen_cabecera"]["type"] == "image/jpg") || ($_FILES["imagen_cabecera"]["type"] == "image/jpeg")) && in_array($file_extension, $valid_extensions)){
-            $sourcePath = $_FILES['imagen_cabecera']['tmp_name'];
-            $targetPath = "uploads/".$fileName;
-            if(move_uploaded_file($sourcePath,$targetPath)){
-                $uploadedFile = $fileName;
+        $uploadedFile = '';
+        if(!empty($_FILES["imagen_cabecera"]["type"])){
+            $fileName = time().'_'.$_FILES['imagen_cabecera']['name'];
+            $valid_extensions = array("jpeg", "jpg", "png");
+            $temporary = explode(".", $_FILES["imagen_cabecera"]["name"]);
+            $file_extension = end($temporary);
+            if((($_FILES["imagen_cabecera"]["type"] == "image/png") || ($_FILES["imagen_cabecera"]["type"] == "image/jpg") || ($_FILES["imagen_cabecera"]["type"] == "image/jpeg")) && in_array($file_extension, $valid_extensions)){
+                $sourcePath = $_FILES['imagen_cabecera']['tmp_name'];
+                $targetPath = "uploads/".$fileName;
+                if(move_uploaded_file($sourcePath,$targetPath)){
+                    $uploadedFile = $fileName;
+                }
             }
         }
-    }
     
-    $escrito = $_POST['parrafo'];
-    $titulo = $_POST['titulo_entrada'];
-    $footer = $_POST['foto-footer'];
-    $len = "es";
-    
-    //insert form data in the database
-    $insert = $mysqli->query("INSERT into `entrada_blog` (`lenguaje`, `titulo`, `imagen_central`, `foto_footer`, `texto`) VALUES ( '".$len."','".$titulo."','".$uploadedFile."','".$footer."','".$escrito."')");
-    
-
+        $escrito = mysqli_real_escape_string($mysqli, $_POST['parrafo']);
+        $titulo =  mysqli_real_escape_string($mysqli, $_POST['titulo_entrada']);
+        $footer = mysqli_real_escape_string($mysqli, $_POST['foto-footer']);
+        $len = "es";
+        
+        //insert form data in the database
+        $insert = $mysqli->prepare("INSERT INTO entrada_blog (lenguaje, titulo, imagen_central, foto_footer, texto) VALUES (?, ?, ?, ?, ?)");
+        $insert->bind_param("sssss",$len, $titulo, $uploadedFile, $footer, $escrito);
+        echo $mysqli->error;
+        
+        $insert->execute();
+        $insert->close();
+        $mysqli->close();
     }
 
 ?>
