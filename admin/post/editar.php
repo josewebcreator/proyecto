aec8ffaf738471a671d4fc3b75ce02fee83cf975
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-14 15:21:17 -0400

    Update editar.php

--- admin/post/editar.php
+++ admin/post/editar.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
 
     if(isset($_GET['id'])){
         require('../cone/conexion.php');
@@ -58,3 +67,6 @@
     }
 
 ?>
+
+</body>
+</html>
