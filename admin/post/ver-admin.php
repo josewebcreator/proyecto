ec1c8a543fe511f65b2aff5a5bd285a021493c43
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-14 09:51:26 -0400

    Update ver-admin.php

--- admin/post/ver-admin.php
+++ admin/post/ver-admin.php
@@ -1,3 +1,21 @@
 <?php
+    if(isset($_GET['id'])){
+        
+        require('../cone/conexion.php');
+        $consulta = $mysqli->prepare("SELECT * FROM entrada_blog WHERE id = ?");
+        $idConsulta = mysqli_real_escape_string($mysqli, $_GET['id']);
+        $consulta->bind_param("i",$idConsulta);
+        $consulta->execute();
+        $res = $consulta->get_result();
+        $consulta->close();
 
+        //print_r($res);
+        //print_r($res->num_rows);
+        
+        if($res->num_rows>0){
+            
+        }
+
+        $mysqli->close();
+    }
 ?>
