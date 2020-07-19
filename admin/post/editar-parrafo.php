f58c7c05082de76aeed5f5314bae7276238acf71
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-19 19:10:44 -0400

    Create editar-parrafo.php

---
+++ admin/post/editar-parrafo.php
@@ -0,0 +1,9 @@
+<?php
+
+    if(isset($_POST['id']) && (empty($_FILES['imagen_parrafo']['name']))){
+
+    }elseif(isset($_POST['id']) && !(empty($_FILES['imagen_parrafo']['name']))){
+        
+    }
+
+?>
