e3616f9bddaedf0140194fe453d84134ac7535b6
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-19 19:32:35 -0400

    Update editar-parrafo.php

--- admin/post/editar-parrafo.php
+++ admin/post/editar-parrafo.php
@@ -2,8 +2,26 @@
 
     if(isset($_POST['id']) && (empty($_FILES['imagen_parrafo']['name']))){
 
+        require('../cone/conexion.php');
+        $consulta = $mysqli->prepare('UPDATE parrafo_blog SET sub_titulo = ?, imagen_parrafo = ?, texto = ? WHERE id = ? AND orden = ?');
+        $titulo = mysqli_real_escape_string($mysqli, $_POST['subtitulo']);
+        $orden = mysqli_real_escape_string($mysqli, $_POST['orden']);
+        $texto = mysqli_real_escape_string($mysqli, $_POST['texto']);
+        $id = mysqli_real_escape_string($mysqli, $_POST['id']);
+        $consulta->bind_param("sssi", $titulo, $foto_footer, $texto, $id);
+        echo $mysqli->error . "12<br>";
+        if($consulta->execute()){
+            echo "exito";
+            $consulta->close();
+        }else{
+            echo $mysqli->error . "16<br>";
+            $consulta->close();
+        }
+
     }elseif(isset($_POST['id']) && !(empty($_FILES['imagen_parrafo']['name']))){
-        
+
+
+
     }
 
 ?>
