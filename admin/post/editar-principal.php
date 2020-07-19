38774220328c45671e291989a415d0f17a3e5434
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-07-19 17:51:52 -0400

    Update editar-principal.php

--- admin/post/editar-principal.php
+++ admin/post/editar-principal.php
@@ -31,31 +31,33 @@
         $file_extension = end($temporary);
         if((($_FILES["imagen"]["type"] == "image/png") || ($_FILES["imagen"]["type"] == "image/jpg") || ($_FILES["imagen"]["type"] == "image/jpeg")) && in_array($file_extension, $valid_extensions)){
             $sourcePath = $_FILES['imagen']['tmp_name'];
-            $targetPath = "uploads/".$fileName;
+            $targetPath = "../publicar/uploads/".$fileName;
             if(move_uploaded_file($sourcePath,$targetPath)){
                 $uploadedFile = $fileName;
             }
         }
         
-        $imagen = $mysqli->prepare('SELECT imagen_central WHERE id_ent = ?');
-        $id = mysql_real_escape_string($mysqli, $_POST['id']);
+        $imagen = $mysqli->prepare('SELECT imagen_central FROM entrada_blog WHERE id_ent = ?');
+        $id = mysqli_real_escape_string($mysqli, $_POST['id']);
+        echo $mysqli->error;
         $imagen->bind_param("i", $id);
         $imagen->execute();
         $rimagen= $imagen->get_result();
         $imagen->close();
 
         $rimagen = $rimagen->fetch_assoc();
-        $ruta = "uploads/". $rimagen['imagen_central'];  
+        $ruta = "../publicar/uploads/". $rimagen['imagen_central'];  
         
+        print_r($ruta) ;
         unlink($ruta);
 
         
 
         $consulta = $mysqli->prepare('UPDATE entrada_blog SET titulo = ?, foto_footer = ?, texto = ?, imagen_central = ? WHERE id_ent = ?');
 
-        $titulo = mysql_real_escape_string($mysqli, $_POST['titulo']);
-        $foto_footer = mysql_real_escape_string($mysqli, $_POST['foto_footer']);
-        $texto = mysql_real_escape_string($mysqli, $_POST['texto']);       
+        $titulo = mysqli_real_escape_string($mysqli, $_POST['titulo']);
+        $foto_footer = mysqli_real_escape_string($mysqli, $_POST['foto-footer']);
+        $texto = mysqli_real_escape_string($mysqli, $_POST['texto']);       
         $consulta->bind_param("ssssi", $titulo, $foto_footer, $texto, $uploadedFile, $id);
 
         echo $mysqli->error . "46<br>";
