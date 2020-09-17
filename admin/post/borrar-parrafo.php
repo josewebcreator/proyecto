fadf984049847f71463985b406db488e63a216fa
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-17 18:20:11 -0400

    Update borrar-parrafo.php

--- admin/post/borrar-parrafo.php
+++ admin/post/borrar-parrafo.php
@@ -1,3 +1,13 @@
 <?php
+    
+    if(isset($_POST['id'])&&isset($_POST['orden'])){
+        echo $_POST['id'];
+        require('../cone/conexion.php');
+        $consulta = $mysqli->prepare("DELETE FROM `parrafo_blog`WHERE `id` = ? AND `orden` = ?;");
+        $id = mysqli_real_escape_string($mysqli, $_POST['id']);
+        $orden = mysqli_real_escape_string($mysqli, $_POST['orden']);
+        $consulta->bind_param("ii", $id, $orden);
+        $consulta->execute();
+    }
 
 ?>
