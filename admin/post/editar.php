a9ba5ee04b8d3e6da1d4c370263a36d371cbf7f7
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-17 18:20:17 -0400

    Update editar.php

--- admin/post/editar.php
+++ admin/post/editar.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/jquery.js"></script>
     <script>
-        function borrarParrafo(id, orden){
-            console.log(id)
-            console.log(orden)
+        function borrarParrafo(ide, ord){
+            console.log(ide, ord)
+            $.post("borrar-parrafo.php",
+                {
+                id : ide,
+                orden: ord
+                },
+                function (){
+                    location.reload()
+                }
+            )
+
         }
     </script>
     <title>Document</title>
@@ -86,7 +96,7 @@
 ?>
     <input type="button" value="" id="btn-editar">
     </div>
-<script src="js/jquery.js"></script>
+
 <script src="js/funciones.js"></script>
 
 </body>
