6bcaa8867269140eb64fc2dfc1a1259a9dde9026
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-18 10:50:50 -0400

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -6,6 +6,24 @@
     <title></title>
 </head>
 <body>
-    
+    <div id="login">
+            <div id="titulo">
+                <h2>Login</h2>
+            </div>
+            <div id="datos">
+                <form action="">
+                    <input type="text" name="usuario" id="usuario" class="datolog">
+                    <br>
+                    <input type="password" name="keyword" id="keyword" class="datolog">
+                    <br>
+                    <input type="button" value="" name="ingresar">
+                </form>
+            </div>
+    </div>
+
+<script src="..\js\jquery.js"></script>
+<script src="js\funciones.js"></script>
+
+
 </body>
 </html>
