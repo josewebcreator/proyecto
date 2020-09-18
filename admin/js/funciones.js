b175d155a34dc25fbe855770016f9e85e597de70
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-18 11:49:56 -0400

    Update funciones.js

--- admin/js/funciones.js
+++ admin/js/funciones.js
@@ -4,14 +4,26 @@ $(document).ready(function () {
         e.preventDefault();
         var check = []
         $("#datos").each(function () {
-            $("form", this).children().each(function () {     
+            $("form", this).children().each(function () {
+               if ($(this).is("#usuario") || $(this).is("#keyword")) {
+
                 if ($(this).val().length>0) {
                     check.push(1)
                 } else {
                     check.push(0)
                 }
+            } 
             })
+            
         })
+
+        console.log(check)
+
+        if (!check.includes(0)) {
+            console.log("ok")
+        } else {
+            alert("Falta usuario o Password")
+        }
     })
 
 });
