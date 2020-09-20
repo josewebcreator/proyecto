41caecf8f262412fd85412dac54720c16a7c9476
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-19 20:27:10 -0400

    Update enviar-log.php

--- admin/enviar-log.php
+++ admin/enviar-log.php
@@ -4,16 +4,24 @@
         require('cone\conexion.php');
         $user = mysqli_real_escape_string($mysqli, $_POST['usuario']);
         $pass = mysqli_real_escape_string($mysqli, $_POST['keyword']);
-        $hash = password_hash($pass, PASSWORD_DEFAULT);
-
-        $consulta = $mysqli->prepare("SELECT * FROM login WHERE 'user' = ?");
-        $consulta->bind_param("s", $user);
+        
+        
+        $consulta = $mysqli->prepare("SELECT * FROM `login` WHERE `user` = ?");
+        $consulta->bind_param("s",$user);
         $consulta->execute();
+        
         $res= $consulta->get_result();
         $consulta->close();
-
+        echo $res->num_rows;
         if(($res->num_rows)>0){
-            //iniciar secion aqui
+           
+            while($check=$res->fetch_assoc()){
+                $hash = $check['hash'];
+            }
+               
+            if(password_verify($pass, $hash)){
+                echo "ok";
+            }
             
             $mysqli->close();
         }else{
