1c064734dc47fce9d27cfce2d1c4c3e50045033c
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-20 15:54:50 -0400

    Update enviar-log.php

--- admin/enviar-log.php
+++ admin/enviar-log.php
@@ -6,29 +6,31 @@
         $pass = mysqli_real_escape_string($mysqli, $_POST['keyword']);
         
         
-        $consulta = $mysqli->prepare("SELECT * FROM `login` WHERE `user` = ?");
+        $consulta = $mysqli->prepare("SELECT * FROM `login` WHERE `usuario` = ?");
         $consulta->bind_param("s",$user);
         $consulta->execute();
         
         $res= $consulta->get_result();
+
         $consulta->close();
 
         if(($res->num_rows)>0){
            
             while($check=$res->fetch_assoc()){
+
                 $hash = $check['hash'];
+                if(password_verify($pass, $hash)){
+                    session_start();
+                    $_SESSION['usuario'] = $check['usuario'];
+                    $_SESSION['token'] = $check['token'];
+                }
+
             }
                
-            if(password_verify($pass, $hash)){
-                session_start();
-                while($check=$res->fetch_assoc()){
 
-                    $_SESSION['usuario'] = $check['user'];
-                    $_SESSION['token'] = $check['token'];
-
-                }
-            }
             
+
+
             $mysqli->close();
         }else{
             //Error aqui
