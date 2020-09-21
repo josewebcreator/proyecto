3baae84421071bb6522b5d0fe2650f15d988ce90
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-21 11:44:26 -0400

    Update index.php

--- admin/post/index.php
+++ admin/post/index.php
@@ -1,15 +1,47 @@
+<?php
+
+    session_start();
+    if(!($_SESSION["usuario"]==null)||!($_SESSION["usuario"]=="")){
+
+        require("..\cone\conexion.php");
+        $consulta = $mysqli->prepare("SELECT * FROM `login` WHERE `usuario` = ?");
+        $user = mysqli_real_escape_string($mysqli, $_SESSION["usuario"]);
+        $token = mysqli_real_escape_string($mysqli, $_SESSION["token"]);
+        $consulta->bind_param("s",$user);
+        $consulta->execute();
+        $res = $consulta->get_result();
+        $consulta->close();
+
+        while($check = $res->fetch_assoc()){
+
+            $checkUser = $check['usuario'];
+            $checktoken = $check['token'];
+
+        }
+
+        $mysqli->close();
+
+        if(($user==$checkUser)&&($token==$checktoken)){ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-    
-    <div id="carga-tabla"></div>
+    </head>
+    <body>
+                
+        <div id="carga-tabla"></div>
 
-    <script src="js\jquery.js"></script>
-    <script src="js\funciones.js"></script>
-</body>
+        <script src="js\jquery.js"></script>
+        <script src="js\funciones.js"></script>
+    </body>
 </html>
+<?php  
+
+        }else{
+            header("location:../../inicio/index.php");
+        }  
+    }else{
+        header("location:../../inicio/index.php");
+    } ?>
