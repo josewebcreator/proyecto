5e4097bbd280914de1d5af9ffd38e41dddfab0fe
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-23 11:17:32 -0400

    Update header.php

---
+++ admin/activos/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <div id="titulo">
+                <h2>Logging Admin</h2>
+            </div>
+
+                <nav id="menu">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                    </ul>
+                </nav>               
+        </div>
+    </header>
