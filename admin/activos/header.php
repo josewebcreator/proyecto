20bd7f059b229554adecd069b8cc2229e581444a
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-09-24 10:10:20 -0400

    Update header.php

--- admin/activos/header.php
+++ admin/activos/header.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/estilos.css">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title><?php echo $tittle;?></title>
 </head>
 <body>
     <header>
         <div class="header-content">
             <div id="titulo">
-                <h2>Logging Admin</h2>
+                <h2><?php echo $tittle;?></h2>
             </div>
 
                 <nav id="menu">
