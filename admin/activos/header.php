4dea504e16aa482a629b506c80d0423777dbcde0
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-10-12 18:19:59 -0400

    Update header.php

--- admin/activos/header.php
+++ admin/activos/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/estilos.css">
     <link rel="stylesheet" href="..\css\bootstrap.min.css">
+    <link rel="stylesheet" href="..\css\all.min.css">
     <script src="js/jquery.js"></script>
     <title><?php echo $tittle;?></title>
 </head>
