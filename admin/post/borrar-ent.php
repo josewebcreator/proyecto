0505bfd275280a33233ef0290629ca74a24a746d
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-10-18 11:58:18 -0400

    Update borrar-ent.php

---
+++ admin/post/borrar-ent.php
@@ -0,0 +1,40 @@
+<?php
+
+     session_start();
+    if(!($_SESSION["usuario"]==null)||!($_SESSION["usuario"]=="")){
+
+        require("..\cone\conexion.php");
+        $consulta = $mysqli->prepare("SELECT * FROM `login` WHERE `usuario` = ?");
+        $user = mysqli_real_escape_string($mysqli, $_SESSION["usuario"]);
+        $token = mysqli_real_escape_string($mysqli, $_SESSION["token"]);
+        $consulta->bind_param("s",$user);
+        $consulta->execute();
+        $res = $consulta->get_result();
+        $consulta->close();
+
+        while($check = $res->fetch_assoc()){
+
+            $checkUser = $check['usuario'];
+            $checktoken = $check['token'];
+
+        }
+
+        $mysqli->close();
+
+        if(($user==$checkUser)&&($token==$checktoken)){
+            require('../cone/conexion.php');
+            $consulta = $mysqli->prepare("UPDATE `entrada_blog` SET `borrado` = '1' where `id_ent` = ?");
+            $id= mysqli_real_escape_string($mysqli, $_GET['id']);
+            $consulta->bind_param("s",$id);
+            $consulta->execute();
+            $consulta->close();
+            $mysqli->close();
+        }else{
+            header("location:../../inicio/index.php");
+        }
+
+    }else{
+        header("location:../../inicio/index.php");
+    }
+
+?>
