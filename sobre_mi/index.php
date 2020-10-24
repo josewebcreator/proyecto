4f2bd5b6bd3da27ee6f20b01621b561e12a61eff
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-10-24 19:36:10 -0400

    Create index.php

---
+++ sobre_mi/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$tittle= "Inicio";
+require("..\activos\header.php");
+
+?>
+
+<div class="container">
+
+    <div class="row">
+        <div id="datos-personales" class="col-3">
+            <h5 id="nombre">
+
+            </h5>
+            <h5 id="ocupacion">
+
+            </h5>
+            
+
+        </div>
+        <div id="intro" class="col-9">
+
+        </div>
+    </div>
+
+</div>
