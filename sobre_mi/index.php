b73c1ad9ed1d0f91d38cf019a902950bf16ee6d1
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-10-24 20:02:00 -0400

    Update index.php

--- sobre_mi/index.php
+++ sobre_mi/index.php
@@ -10,12 +10,63 @@ require("..\activos\header.php");
     <div class="row">
         <div id="datos-personales" class="col-3">
             <h5 id="nombre">
-
+                Jose Manuel Garcia Garcia
             </h5>
             <h5 id="ocupacion">
-
+                Estudiante||Porgramador||Diseñador
             </h5>
-            
+    
+            <h4>
+                ¿Que me gusta?
+            </h4>
+
+            <ul id="gustos">
+                <li>Cine</li>
+                <li>Series</li>
+                <li>Aprender cosas nuevas</li>
+            </ul>
+
+            <h4>
+                Ubuntu
+            </h4>
+
+            <p>
+                Una persona no mejora sola, lo hace cuando ayuda a los demas a ser mejores, y viceversa.
+            </p>
+
+            <h4>Conocimientos</h4>
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                    <th scope="col">Area</th>
+                    <th scope="col">Nivel</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">HTML5</th>
+                    <td>Intermedio</td>
+
+                    </tr>
+                    <tr>
+                    <th scope="row">PHP</th>
+                    <td>Intermedio</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Javascrip</th>
+                    <td>Intermedio</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Jquery</th>
+                    <td>Principiante</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Bootstrap</th>
+                    <td>Intermedio</td>
+                    </tr>
+                </tbody>
+                </table>
 
         </div>
         <div id="intro" class="col-9">
