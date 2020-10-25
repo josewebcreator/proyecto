17e1b523cf6bf4ceaa1be57c80f8ee1f5eddccd6
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-10-25 11:07:40 -0400

    Update index.php

--- sobre_mi/index.php
+++ sobre_mi/index.php
@@ -67,10 +67,26 @@ require("..\activos\header.php");
                     </tr>
                 </tbody>
                 </table>
-
+            
         </div>
-        <div id="intro" class="col-9">
-
+        <div class="col-1"></div>
+        <div id="intro" class="col-8">
+            <h2>¿Quien Soy?</h2>
+            <p>Antes de todo bienvenido</p>
+            <p>Soy un estudiante de Ingeniería en sistemas, incursionando en el maravilloso mundo de diseño web y la programación. Me considero una persona cuya vida le ha enseñado a ser centrada, humilde y a aceptar todo tipo de aprendizaje.</p>
+            <p>Algunas características que me gustan de mi es que soy un jugador en equipo, me gusta ayudar a mis compañeros y no temo a preguntar.</p>
+            <p>Me considero un entusiasta de la tecnología, capaz de aprender muy rápidamente como de implementar dichos conocimientos.</p>
+            <p>He creado desde cero este sitio web, planeando, creando y codificando a mano la aplicación para la creación de entradas de blog que hay en el mismo, por lo tanto, aunque no tengo experiencia en alguna empresa, me siento capaz de asumir cualquier reto que este camino me plantee.</p>
+            <h3>Un poco de este web site</h3>
+            <p>La idea principal era presentarme como programador y persona, pero, con el tiempo me enseño a seguir un plan de trabajo y a complementar los conocimientos que ya había adquirido.</p>
+            <p>Me puse manos a la obra y no solo plantie un sitio para publicitarme, si no tambien, algo que quedara en la red y pudiera servir de base para el proyecto de alguien más.</p>
+            <p>Así es pues, plantie, un web site, que pudiera servir, a aquellas personas sin casi conocimiento de diseño como plantilla y aplicación para la publicacion de entradas de blogs y noticias sin casi ningun esfuerzo alguno, incorporando mis conocimientos de MySql y PHP.</p>
+            <a href="#">colocar link de github</a>
+        </div>
+        <div class="col-12"></div>
+        <div class="col-12">
+            <h2>Listo para el reto</h2>
+            
         </div>
     </div>
 
