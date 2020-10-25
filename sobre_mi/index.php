79800e074b4c6325c7bd45fef882be3482774b83
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-10-25 11:27:25 -0400

    Update index.php

--- sobre_mi/index.php
+++ sobre_mi/index.php
@@ -31,7 +31,8 @@ require("..\activos\header.php");
             </h4>
 
             <p>
-                Una persona no mejora sola, lo hace cuando ayuda a los demas a ser mejores, y viceversa.
+                Una persona es una persona a causa de los demás.
+                Una persona no mejora sola, lo hace cuando ayuda a los demas a ser mejores.
             </p>
 
             <h4>Conocimientos</h4>
@@ -77,15 +78,17 @@ require("..\activos\header.php");
             <p>Algunas características que me gustan de mi es que soy un jugador en equipo, me gusta ayudar a mis compañeros y no temo a preguntar.</p>
             <p>Me considero un entusiasta de la tecnología, capaz de aprender muy rápidamente como de implementar dichos conocimientos.</p>
             <p>He creado desde cero este sitio web, planeando, creando y codificando a mano la aplicación para la creación de entradas de blog que hay en el mismo, por lo tanto, aunque no tengo experiencia en alguna empresa, me siento capaz de asumir cualquier reto que este camino me plantee.</p>
-            <h3>Un poco de este web site</h3>
+            <h3>Un poco sobre este web site</h3>
             <p>La idea principal era presentarme como programador y persona, pero, con el tiempo me enseño a seguir un plan de trabajo y a complementar los conocimientos que ya había adquirido.</p>
             <p>Me puse manos a la obra y no solo plantie un sitio para publicitarme, si no tambien, algo que quedara en la red y pudiera servir de base para el proyecto de alguien más.</p>
             <p>Así es pues, plantie, un web site, que pudiera servir, a aquellas personas sin casi conocimiento de diseño como plantilla y aplicación para la publicacion de entradas de blogs y noticias sin casi ningun esfuerzo alguno, incorporando mis conocimientos de MySql y PHP.</p>
             <a href="#">colocar link de github</a>
         </div>
         <div class="col-12"></div>
-        <div class="col-12">
+        <div class="col-12 cierre-bio">
             <h2>Listo para el reto</h2>
+            <p>Estoy listo para comprometerme con cualquier reto que esta labor me exija.</p>
+            <p><i>"No debemos abandonar nunca en la búsqueda de nuestras metas personales, con tiempo y paciencia lo lograremos."</i> <b>Norman Vincent Peale</b></p>
             
         </div>
     </div>
