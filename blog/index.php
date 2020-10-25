a7dfbe96e5e9987465f60e5826ccef201b3002c9
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-10-25 12:42:34 -0400

    Update index.php

--- blog/index.php
+++ blog/index.php
@@ -5,3 +5,22 @@ require("..\activos\header.php");
 
 ?>
 
+<div class="container" id="entradas">
+    <div class="row">
+
+        <div class="col-12 row" id="principales">
+            <div class="col-1"></div>
+            <div class="col-5 ultimas">
+                <div class="imagen"></div>
+                <h3>titulo principal</h3>
+            </div>
+            <div class="col-1"></div>
+            <div class="col-5 ultimas">
+                <div class="imagen"></div>
+                <h3>titulo principal</h3>
+            </div>
+    
+        </div>
+        
+    </div>
+</div>
