e0e6f21f59e6a1f882cfac6e40e54b1892728d6e
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-11-16 15:37:49 -0400

    Update footer.php

--- admin/post/footer.php
+++ admin/post/footer.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="menu-elem-footer col-1">
                 <div class="link-background-footer">
-                    <a href="index.php" class="link-menu-footer" title="regresar al menu"><i class="fas fa-home"></i></a>
+                    <a href="javascript:history.back();" class="link-menu-footer" title="regresar al menu"><i class="fas fa-arrow-left"></i></a>
                 </div>                    
             </div>  
         </div>
