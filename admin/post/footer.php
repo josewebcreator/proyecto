ddae82e3f3d7210a79de1532c887eedcd1694113
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-11-16 14:51:33 -0400

    Create footer.php

---
+++ admin/post/footer.php
@@ -0,0 +1,13 @@
+<div class="footer">
+
+    <div class="container" id="menufooter">
+        <div class="row">
+            <div class="menu-elem-footer col-1">
+                <div class="link-background-footer">
+                    <a href="index.php" class="link-menu-footer" title="regresar al menu"><i class="fas fa-arrow-left"></i></a>
+                </div>                    
+            </div>  
+        </div>
+    </div>
+
+</div>
