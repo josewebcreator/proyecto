1bf64ccd6aadc2677e4e95cd473a2c3a46802aa0
josewebcreator <65829211+josewebcreator@users.noreply.github.com>
2020-11-16 14:26:32 -0400

    Update papelera.php

--- admin/post/papelera.php
+++ admin/post/papelera.php
@@ -21,75 +21,102 @@
 
         $mysqli->close();
 
-        if(($user==$checkUser)&&($token==$checktoken)){ ?>
+        if(($user==$checkUser)&&($token==$checktoken)){ 
+            require('../cone/conexion.php');
+            $consulta = $mysqli->prepare("SELECT id FROM `login` WHERE `usuario` = ?");
+            $consulta->bind_param("s",$user);
+            $consulta->execute();
+            $res = $consulta->get_result();
+                while($row = $res->fetch_assoc()){
+                    $id_log = $row['id'];
+                }
+            $consulta->close();
 
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="../css/estilos.css">
-            <link rel="stylesheet" href="..\css\bootstrap.min.css">
-            <script src="js/jquery.js"></script>
-            <title>Papelera</title>
-        </head>
-        <body>
-            <header>
-                <div class="header-content">
-                    <div id="titulo">
-                        <h2>Papelera</h2>
-                    </div>
+            $consulta = $mysqli->prepare("SELECT * FROM `usuario` WHERE `id_login` = ?");
+            $consulta->bind_param("i",$id_log);
+            $consulta->execute();
+            $res = $consulta->get_result();
+            $total=$res->num_rows;
+            $consulta->close();
 
-                        <nav id="menu">
-                            
-                        </nav>               
-                </div>
-            </header>
+            while($row = $res->fetch_assoc()){
+                $tipo = $row['tipo'];
+            }
+
+            if($tipo=="admin"){
+                $tittle = "Papelera";
+            }
+            ?>
+
+        
             <?php
             
             require('../cone/conexion.php');
+            $consulta = $mysqli->prepare("SELECT id FROM `login` WHERE `usuario` = ?"); 
+            $consulta->bind_param("s",$user);
+            $consulta->execute();
+            $res = $consulta->get_result();
+                while($row = $res->fetch_assoc()){
+                    $id_log = $row['id'];
+                }
+            $consulta->close();
 
-            $consulta = $mysqli->query("SELECT * from entrada_blog WHERE `borrado` = '1'");
-            $cuenta =  $consulta->num_rows;
-        
-            if ($cuenta>0){
-                ?>
-                <br>
-                <br>
-                <div class="cuadro table-responsive-sm">
-                  <table class="table table-dark">
-                    <tr>
-                        <td scope="col" style="text-align: center;">Id</td>
-                        <td scope="col" style="text-align: center;">idioma</td>
-                        <td scope="col" style="text-align: center;">Titulo</td>
-                        <td scope="col" style="text-align: center;">Opciones</td>
-                    </tr>
-                    <?php
-                        while ($row = $consulta->fetch_assoc()){
-                            ?>
-                            <tr>
-                                <td style="text-align: center;"><?php echo $row['id_ent']; ?></td>
-                                <td><?php echo $row['lenguaje']; ?></td>
-                                <td><?php echo $row['titulo']; ?></td>
-                                <td style="text-align: center;">
-                                    
-                                    <a href="#" refe="<?php echo $row['id_ent']; ?>" onclick="restaura(<?php echo $row['id_ent']; ?>)">Restaurar</a>
-                                    <a href="#" class="btn-borrar" refe="<?php echo $row['id_ent']; ?>" onclick="borrar_final(<?php echo $row['id_ent']; ?>)">Borrar</a>
-                                    
-                                </td>
-                            </tr>
-                        <?php }//fin del while
-                    ?>
-                </table>  
-                </div>
-                
-                <?php //fin IF
-                $consulta->close();
-            }else{
-                
-                echo "No existen publicaciones"; 
-                $consulta->close();
+            $consulta = $mysqli->prepare("SELECT * FROM `usuario` WHERE `id_login` = ?");
+            $consulta->bind_param("i",$id_log);
+            $consulta->execute();
+            $res = $consulta->get_result();
+            $total=$res->num_rows;
+            $consulta->close();
+
+            while($row = $res->fetch_assoc()){
+                $tipo = $row['tipo'];
             }
+
+            if($tipo=="admin"){
+                require("../activos/header.php");
+                $consulta = $mysqli->query("SELECT * from entrada_blog WHERE `borrado` = '1'");
+                $cuenta =  $consulta->num_rows;
+            
+                if ($cuenta>0){
+                    ?>
+                        <div class="cuadro table-responsive-sm">
+                        <table class="table table-dark">
+                            <tr>
+                                <td scope="col" style="text-align: center;">Id</td>
+                                <td scope="col" style="text-align: center;">idioma</td>
+                                <td scope="col" style="text-align: center;">Titulo</td>
+                                <td scope="col" style="text-align: center;">Opciones</td>
+                            </tr>
+                            <?php
+                                while ($row = $consulta->fetch_assoc()){
+                                    ?>
+                                    <tr>
+                                        <td style="text-align: center;"><?php echo $row['id_ent']; ?></td>
+                                        <td><?php echo $row['lenguaje']; ?></td>
+                                        <td><?php echo $row['titulo']; ?></td>
+                                        <td style="text-align: center;">
+                                            
+                                            <a href="#" refe="<?php echo $row['id_ent']; ?>" onclick="restaura(<?php echo $row['id_ent']; ?>)">Restaurar</a>
+                                            <a href="#" class="btn-borrar" refe="<?php echo $row['id_ent']; ?>" onclick="borrar_final(<?php echo $row['id_ent']; ?>)">Borrar</a>
+                                            
+                                        </td>
+                                    </tr>
+                                <?php }//fin del while
+                            ?>
+                        </table>  
+                        </div>
+                    
+                    <?php //fin IF
+                    $consulta->close();
+                }else{
+                    
+                    echo "No existen publicaciones"; 
+                    $consulta->close();
+                }
+            }else{
+                header("location:index.php");
+            }
+            
 
             $mysqli->close();
 
